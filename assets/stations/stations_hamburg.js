4852b1551f85a99ae3e05c7b0ba17296ec1d7761
flixxgamez <71939976+flixxgamez@users.noreply.github.com>
2022-01-29 18:25:47 +0100

    Add files via upload

---
+++ assets/stations/stations_hamburg.js
@@ -0,0 +1,29 @@
+const stations = [
+    {
+        name: 'Hamburg Altona',
+        region: 'Hamburg',
+        eva: '8002553'
+    },
+    {
+        name: 'Hamburg Bergedorf',
+        region: 'Hamburg',
+        eva: '8002554'
+    },
+    {
+        name: 'Hamburg Dammtor',
+        region: 'Hamburg',
+        eva: '8002548'
+    },
+    {
+        name: 'Hamburg Harburg',
+        region: 'Hamburg',
+        eva: '8000147'
+    },
+    {
+        name: 'Hamburg Hbf',
+        region: 'Hamburg',
+        eva: '8002549'
+    },
+]
+
+exports.stations = stations;
