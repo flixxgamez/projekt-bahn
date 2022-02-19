9c76f15f4e43787eb2bc85005e15668b58540dbc
flixx <71939976+flixxgamez@users.noreply.github.com>
2022-02-19 12:38:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+//Code von flixx - Open Source » https://github.com/flixxgamez/projekt-bahn
+const createClient = require('db-hafas');
+const hafasClient = createClient('https://github.com/flixxgamez/projekt-bahn');
+
+//Für das Beispiel von Berlin (siehe unter station_data/stations_berlin). Kann durch Dateien mit gleichem Aufbau ausgetauscht werden.
+const { stationData } = require('./station_data/stations_schleswig-holstein');
+const { createDir } = require('./createDir');
+createDir(stationData, './data')
+console.log(`Projekt-Bahn 〣 Das Script wurde erfolgreich für die Stationsdaten "${stationData.regionName}" gestartet \n`);
+
+//Importiert weitere Loggingtools
+const DiscordJS = require('discord.js');
+const discordClient = new DiscordJS.Client();
+
+discordClient.login('OTMyMjk2NzU5NjUxMjA5MjE2.YeQ7BA.U1XBQOyR9mKff_9VVpIC1kImfd0'); //Hier eigenen Token für den Discord-Bot einfügen
+const { logStation, logStop, saveData } = require('./logging');
+
+//Loop die in einem Regelmäßigen Abstand Abfahrten abfragt, 'difference' gibt Abstand zwischen 2 Durchläufen in min an
+const difference = 1.5;
+const intervalDuration = difference * 60000 / stationData.stations.length;
+let i = 0;
+
+setInterval(() => {
+
+    //Lädt die relevanten Stationsdaten
+    const requestStation = {
+        name: stationData.stations[i].name,
+        eva: stationData.stations[i].eva,
+        region: stationData.stations[i].region,
+    }
+
+    if(i === stationData.stations.length - 1) {i = 0} else i++
+
+    logStation(requestStation)
+
+    //Fragt die nächste Abfahrten an einem Bahnhof ab (ersetze "depatures" durch "arrivals" für Ankünfte)
+    //Art der Verkehrmittel, die angefragt werden sollen können individuel geändert werden
+    //Für mehr Infos siehe https://github.com/public-transport/hafas-client/blob/master/docs/departures.md
+    hafasClient.departures(requestStation.eva, {duration: difference, remarks: true, products: {tram: false, bus: false, ferry: false, subway: false, suburban: false}})
+        .catch(err => console.log(err))
+        .then(res => {
+
+            saveData(res, './data', requestStation);
+
+            if(res.length != 0) {
+              
+                res.map(stop => {
+
+                    //"stop" stellt für den jeden Zug aus der Abfrage die Daten dar
+                    if(stop.line.mode === 'train') {
+                        logStop(DiscordJS, discordClient, stop, requestStation, stationData.discordID);
+                    }
+                                        
+                });
+
+            } 
+            
+        })
+
+}, intervalDuration)
